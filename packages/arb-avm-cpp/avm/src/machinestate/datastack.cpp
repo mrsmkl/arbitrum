3c097739476f4effc89f1540b017938b3a4e0662
Harry Kalodner <harry@offchainlabs.com>
2020-05-28 15:38:49 -0400

    Fix to_big_endian and add tests

--- packages/arb-avm-cpp/avm/src/machinestate/datastack.cpp
+++ packages/arb-avm-cpp/avm/src/machinestate/datastack.cpp
@@ -117,17 +117,10 @@ void Datastack::addHash() const {
     tupData[0] = TUPLE + 2;
     ++oit;
     auto valHash = ::hash(values[hashes.size()]);
-    std::array<uint64_t, 4> valHashInts;
-    to_big_endian(valHash, valHashInts.begin());
-    std::copy(reinterpret_cast<unsigned char*>(valHashInts.data()),
-              reinterpret_cast<unsigned char*>(valHashInts.data()) + 32, oit);
-    oit += 32;
-    std::array<uint64_t, 4> valHashInts2;
-    to_big_endian(prev, valHashInts2.begin());
-    std::copy(reinterpret_cast<unsigned char*>(valHashInts2.data()),
-              reinterpret_cast<unsigned char*>(valHashInts2.data()) + 32, oit);
+    oit = to_big_endian(valHash, oit);
+    to_big_endian(prev, oit);
     std::array<unsigned char, 32> hashData;
-    evm::Keccak_256(tupData.data(), 1 + 32 * 2, hashData.data());
+    evm::Keccak_256(tupData.data(), tupData.size(), hashData.data());
     hashes.emplace_back(from_big_endian(hashData.begin(), hashData.end()));
 }
 
